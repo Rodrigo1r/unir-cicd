b153446503abd7c4e963317a1ed297856aad3a87
Rodrigo <rodrigo@Ubuntu2004.myguest.virtualbox.org>
2024-06-14 22:27:15 -0500

    agregar directorios

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -7,32 +7,36 @@ pipeline {
             steps {
                 echo 'Building stage!'
                 sh 'make build'
+                
             }
         }
         stage('Unit tests') {
             steps {
                 sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
+                archiveArtifacts artifacts: 'results/unit_test/*.xml'
             }
         }
         stage('Pruebas API') {
             steps {
                 sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
+                archiveArtifacts artifacts: 'results/api_test/*.xml'
             }
         }
 
         stage('Pruebas E2E') {
             steps {
+                sh 'make server'
                 sh 'make test-e2e'
-                archiveArtifacts artifacts: 'results/*.xml'
+                archiveArtifacts artifacts: 'results/e2e_test/*.xml'
             }
         }
 
     }
     post {
         always {
-            junit 'results/*_result.xml'
+            junit 'results/unit_test/*_result.xml'
+            junit 'results/api_test/*_result.xml'
+            junit 'results/e2e_test/*_result.xml'
             cleanWs()
         }
     }
